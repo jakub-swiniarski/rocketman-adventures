f6542504410f07e10400e85aa294651494083b5f
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-09-18 17:40:32 +0200

    fullscreen

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int main(void){
     const int screenHeight=720;
 
     InitWindow(screenWidth,screenHeight,"Rocketman Adventures");
-
+    
+    int display=GetCurrentMonitor();
+    SetWindowSize(GetMonitorWidth(display),GetMonitorHeight(display));
+    ToggleFullscreen();
+    
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
