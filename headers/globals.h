0db50f47c5f592bb0c569b9e2e9ba0f0ffc4c48a
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-09-25 17:45:51 +0200

    include guard for globals

--- headers/globals.h
+++ headers/globals.h
@@ -1,3 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
 static const char *directory="assets/";
 static const unsigned short screenWidth=1280;
 static const unsigned short screenHeight=720;
+
+#endif
