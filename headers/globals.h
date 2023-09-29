e84cc881960cf2e456a4952e79299677323a06ff
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-09-29 17:20:49 +0200

    increased resolution and amount of platforms

--- headers/globals.h
+++ headers/globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 
 static const char *directory="assets/";
-static const unsigned short screenWidth=1280;
-static const unsigned short screenHeight=720;
+static const unsigned short screenWidth=1920;
+static const unsigned short screenHeight=1080;
 
 #endif
