d06cd842b31caa9938f8308edb6c5b4ab302cdaa
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-10-03 17:25:54 +0200

    make uninstall install targets

--- headers/globals.h
+++ headers/globals.h
@@ -1,7 +1,7 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-static const char *directory="assets/";
+static const char *directory="/usr/share/assets_rocketman/";
 static const unsigned short screenWidth=1920;
 static const unsigned short screenHeight=1080;
 static const char *version="1.0.0";
