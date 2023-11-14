243f65e2d59617828dd6db0c946a7e1b023062a0
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-11-14 17:45:35 +0100

    1.3.1

--- src/globals.h
+++ src/globals.h
@@ -1,9 +1,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-static const char *DIRECTORY="res/";
+static const char *DIRECTORY="/usr/share/rocketman/";
 static const unsigned short SCREENWIDTH=1920;
 static const unsigned short SCREENHEIGHT=1080;
-static const char *VERSION="1.3.0";
+static const char *VERSION="1.3.1";
 
 #endif
