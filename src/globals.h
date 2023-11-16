e9bd43f871a86207f407e69bbdf49cf2374a9c0c
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-11-16 20:40:13 +0100

    temporary hud

--- src/globals.h
+++ src/globals.h
@@ -7,7 +7,7 @@ typedef int8_t i8;
 typedef uint8_t ui8;
 typedef unsigned short us;
 
-static const char *DIRECTORY="/usr/share/rocketman/";
+static const char *DIRECTORY="res/";
 static const unsigned short SCREENWIDTH=1920;
 static const unsigned short SCREENHEIGHT=1080;
 static const char *VERSION="1.3.3";
