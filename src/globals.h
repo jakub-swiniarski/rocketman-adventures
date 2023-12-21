e683292cb9d544b2f30753830552870a980efc12
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-12-21 08:46:31 +0100

    update header

--- src/globals.h
+++ src/globals.h
@@ -4,8 +4,8 @@
 static const int MAXROCKETS=3;
 static const int MAXPARTICLES=MAXROCKETS*2;
 
-static const char *DIRECTORY="res/";
-static const char *VERSION="1.5.3";
+static const char *DIRECTORY="/usr/local/share/rocketman/";
+static const char *VERSION="1.5.4";
 
 #define SCREENMIDDLE(X) (int)(SCREENHEIGHT/2)-(int)(X.tx.height/2)
 #define MIDDLEX(X) ((int)(X.tx.width/2))
