ed6be824173fe1080e69770e88d0e6753c22164e
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-12-27 18:48:29 +0100

    ready to release

--- src/globals.h
+++ src/globals.h
@@ -5,7 +5,7 @@ static const int MAXROCKETS=3;
 static const int MAXPARTICLES=MAXROCKETS*2;
 static const int NUMBG=9;
 
-static const char *DIRECTORY="res/";
-static const char *VERSION="1.5.6";
+static const char *DIRECTORY="/usr/local/share/rocketman/";
+static const char *VERSION="1.6.0";
 
 #endif
