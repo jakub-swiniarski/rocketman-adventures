3004f02cf066e5b699344aec194d77a75b3e89de
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-01-07 12:41:02 +0100

    ready to release

--- src/globals.h
+++ src/globals.h
@@ -6,7 +6,7 @@ static const int MAX_PARTICLES=MAX_ROCKETS*2;
 #define NUM_BG 9
 #define NUM_PICKUP 2
 
-static const char *DIRECTORY="res/";
-static const char *VERSION="1.7.0";
+static const char *DIRECTORY="/usr/local/share/rocketman/";
+static const char *VERSION="1.7.1";
 
 #endif
