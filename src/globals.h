d91863e1376332dc49983122e69daf9d8f715413
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-02-06 13:13:59 +0100

    new sfx system

--- src/globals.h
+++ src/globals.h
@@ -4,11 +4,13 @@
 #define NUM_BG 11
 #define NUM_PICKUP 2
 #define NUM_MUSIC 3
+#define NUM_SFX 4
 
-enum {STANDING, WALKING, JUMPING};
-enum {MENU, IN_PROGRESS, OVER};
-enum {NONE, PARACHUTE, CRIT};
-enum {NORMAL, HOVER};
+enum {STANDING, WALKING, JUMPING}; //player states, used for animations
+enum {MENU, IN_PROGRESS, OVER}; //game states
+enum {NONE, PARACHUTE, CRIT}; //pickups
+enum {NORMAL, HOVER}; //button states
+enum {SFX_EXPLOSION, SFX_PICKUP, SFX_JUMP, SFX_DEATH}; //sound effects
 
 static const char *DIRECTORY="res/";
 static const char *VERSION="3.0.0";
