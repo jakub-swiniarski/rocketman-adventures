61b87c43cc541b55a41e237338574c288f53ec04
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-02-19 18:50:50 +0100

    move defines to enums

--- globals.h
+++ globals.h
@@ -2,15 +2,13 @@
 #define GLOBALS_H
 
 #define NUM_BG 11
-#define NUM_PICKUP 2
 #define NUM_MUSIC 3
-#define NUM_SFX 4
 
 enum {STANDING, WALKING, JUMPING}; //player states, used for animations
 enum {MENU, IN_PROGRESS, OVER}; //game states
-enum {NONE, PARACHUTE, CRIT}; //pickups
+enum {NONE, PARACHUTE, CRIT, NUM_PICKUP}; //pickups
 enum {NORMAL, HOVER}; //button states
-enum {SFX_EXPLOSION, SFX_PICKUP, SFX_JUMP, SFX_DEATH}; //sound effects
+enum {SFX_EXPLOSION, SFX_PICKUP, SFX_JUMP, SFX_DEATH, NUM_SFX}; //sound effects
 
 static const char *DIRECTORY="res/";
 static const char *VERSION="3.0.1";
